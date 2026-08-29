6f631c4b6b65125a38ed392fe0cd7857dc50bca6
Webzod <you@example.comdiegopalmar99@gmail.comgit config --global user.name Your>
2026-08-29 18:04:56 -0400

    first commit

---
+++ fundamentos.py
@@ -0,0 +1,22 @@
+name = "Josefina"
+print(name)
+last_name = "González" 
+#valor de PI
+PI = 3.14159
+#operadores matematicos
+suma1 = 5+3
+num1= 12
+num2= 30
+suma2 = num1 + num2
+resta1 = num1 - num2
+multi = num1 * num2
+div= num1 / num2
+
+
+
+print(suma2)
+print(multi)
+print(div)
+print(resta1)
+
+
